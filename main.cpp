a17efa2b8322ad8248fedb9a3f5b58603480023e
pabloflores465 <pablopolis2016@gmai.com>
2024-09-20 19:36:32 -0600

    limpiado un poco el codigo

--- main.cpp
+++ main.cpp
@@ -2,33 +2,33 @@
 #include <SFML/Graphics.hpp>
 
 using std::cout, std::cin, std::endl;
-using sf::RenderWindow, sf::VideoMode, sf::CircleShape, sf::Event;
+using sf::RenderWindow, sf::VideoMode, sf::CircleShape, sf::Event, sf::Color;
 
 int main() {
     // Crear una ventana con un tamaño de 800x600 píxeles
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Ventana SFML");
+    RenderWindow window(VideoMode(800, 600), "Ventana SFML");
 
     // Crear un círculo con un radio de 50 píxeles
-    sf::CircleShape circle(50);
+    //sf::CircleShape circle(50);
     // Establecer el color del círculo a verde
-    circle.setFillColor(sf::Color::Green);
+    //circle.setFillColor(sf::Color::Green);
 
     // Bucle principal de la aplicación
     while (window.isOpen())
     {
-        sf::Event event;
+        Event event;
         // Procesar los eventos de la ventana (como cerrar)
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if (event.type == Event::Closed)
                 window.close();
         }
 
         // Limpiar la ventana con un color negro
-        window.clear(sf::Color::Black);
+        window.clear(Color::Black);
 
         // Dibujar el círculo en la ventana
-        window.draw(circle);
+        //window.draw(circle);
 
         // Mostrar lo que se ha dibujado hasta ahora
         window.display();
