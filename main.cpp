46a679830ae2e3e73d16f1f25a365031b9547f8f
pabloflores465 <pablopolis2016@gmai.com>
2024-09-20 23:01:08 -0600

    funcion para dibujar hacientos

--- main.cpp
+++ main.cpp
@@ -3,16 +3,34 @@
 #include <SFML/Graphics.hpp>
 
 using std::cout, std::cin, std::endl;
-using sf::RenderWindow, sf::VideoMode, sf::CircleShape, sf::Event, sf::Color;
+using sf::RenderWindow, sf::VideoMode, sf::CircleShape, sf::RectangleShape, sf::Vector2f, sf::Event, sf::Color;
+
+struct drawSeatParameters {
+    RenderWindow* window;
+    Color fillColor;
+};
+
+void* drawSeat(void* args){
+    drawSeatParameters* params = static_cast<drawSeatParameters*>(args);
+    RenderWindow* window = params->window;
+    Color fillColor = params->fillColor;
+    RectangleShape rectangle(Vector2f(25.f, 25.f));
+    rectangle.setFillColor(fillColor);
+    window->draw(rectangle);
+    return NULL;
+}
 
 int main() {
     // Crear una ventana con un tamaño de 800x600 píxeles
     RenderWindow window(VideoMode(800, 800), "Segundo Examen Parcial Pablo Flores, Randy Rivera, Katherine Dieguez");
 
-    // Crear un círculo con un radio de 50 píxeles
-    CircleShape circle(50);
-    // Establecer el color del círculo a verde
-    circle.setFillColor(Color::Green);
+    drawSeatParameters params;
+
+    params.window = &window;
+    params.fillColor = Color::Red;
+
+    pthread_t thread;
+    pthread_create(&thread, NULL, drawSeat, &params);
 
     // Bucle principal de la aplicación
     while (window.isOpen())
@@ -28,12 +46,11 @@ int main() {
         // Limpiar la ventana con un color negro
         window.clear(Color::Black);
 
-        // Dibujar el círculo en la ventana
-        window.draw(circle);
-
         // Mostrar lo que se ha dibujado hasta ahora
         window.display();
     }
+
+    pthread_join(thread, NULL);
     return 0;
 }
 
